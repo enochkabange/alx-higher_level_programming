afeb71d3490ecc3aae799e18dc12af9c09462e1c
Enoch W. Kabange <enochkabange@gmail.com>
2024-10-04 06:35:45 -0700

    worked on a couple of tasks in the 0x00 python project on high level programming specifally the task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]  # First 3 letters (index 0 to 2)
-word_last_2 = word[-2:]  # Last 2 letters (index -2 to the end)
-middle_word = word[1:-1]  # Middle word (from index 1 to -1, excluding first and last letters)
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+word_first_3 = word[:3]  # Slices the first 3 letters
+word_last_2 = word[-2:]  # Slices the last 2 letters
+middle_word = word[1:-1]  # Slices the word, excluding the first and last letters
+print("First 3 letters: {}".format(word_first_3))  # Prints the first 3 letters
+print("Last 2 letters: {}".format(word_last_2))  # Prints the last 2 letters
+print("Middle word: {}".format(middle_word))  # Prints the middle part of the word
